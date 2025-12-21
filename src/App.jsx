9a83e3d438a176e4f8c5aa0cc28244c2caf4ad0a
Harry Kao <harry@hairycow.name>
2025-12-21 07:15:09 -0800

    Improve introspection query.
    Steal the one from `graphql-explorer`.
    
    Also determine if fields require arguments.

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,115 @@ import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 
 const QUERY = gql`
-  query {
+  query IntrospectionQuery {
     __schema {
       queryType {
+        ...FullType
+      }
+      mutationType {
         name
-        fields(includeDeprecated: true) {
+      }
+      subscriptionType {
+        name
+      }
+      types {
+        ...FullType
+      }
+      directives {
+        name
+        description
+        locations
+        args {
+          ...InputValue
+        }
+      }
+    }
+  }
+
+  fragment FullType on __Type {
+    kind
+    name
+    description
+    fields(includeDeprecated: true) {
+      name
+      description
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+      isDeprecated
+      deprecationReason
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+      description
+      isDeprecated
+      deprecationReason
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+
+  fragment InputValue on __InputValue {
+    name
+    description
+    type {
+      ...TypeRef
+    }
+    defaultValue
+  }
+
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
           name
-          description
-          type {
-            name
+          ofType {
             kind
-            ofType {
-              name
-              kind
-            }
-          }
-          args {
             name
-            description
-            type {
-              name
+            ofType {
               kind
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                }
+              }
             }
           }
         }
       }
-      types {
-        name
-      }
     }
   }
 `
+
+function fieldRequiresInputs(field) {
+  for (const arg of field.args) {
+    if ((arg.type.kind === "NON_NULL") && (arg.defaultVaule == null)) {
+      return true
+    }
+  }
+  return false
+}
 
 function App() {
   const { loading, error, data } = useQuery(QUERY);
@@ -43,7 +121,13 @@ function App() {
 
   return (
     <>
-      {data.__schema.queryType.fields.map(f => (<div key={f.name}>{f.name}</div>))}
+      {data.__schema.queryType.fields.map(f => {
+        if (fieldRequiresInputs(f)) {
+          return <div key={f.name}>{f.name}</div>
+        } else {
+          return <div key={f.name}>{f.name} (callable)</div>
+        }
+      })}
     </>
   )
 }
