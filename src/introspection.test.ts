1fffb94315a728880e57633f0aff758f87c5a9c0
Harry Kao <harry@hairycow.name>
2026-01-01 13:19:29 -0800

    Add tests for extracting type defs.

--- src/introspection.test.ts
+++ src/introspection.test.ts
@@ -1,3 +1,115 @@
-it("succeeds", () => {
-    //
+import {
+    IntrospectionListTypeRef,
+    IntrospectionNonNullTypeRef,
+    IntrospectionScalarType,
+} from "graphql";
+import { GqlTypeDef, extractTypeInformation } from "./introspection";
+
+describe("extractTypeInformation", () => {
+    it("parses nullable scalar", () => {
+        const typeSchema: IntrospectionScalarType = {
+            name: "TypeName",
+            kind: "SCALAR",
+        };
+        const expected: GqlTypeDef = {
+            name: "TypeName",
+            kind: "SCALAR",
+            isNullable: true,
+            isList: false,
+            isListNullable: true,
+        };
+
+        const typeDef = extractTypeInformation(typeSchema);
+        expect(typeDef).toStrictEqual(expected);
+    });
+
+    it("parses non-nullable scalar", () => {
+        const typeSchema: IntrospectionNonNullTypeRef<IntrospectionScalarType> = {
+            kind: "NON_NULL",
+            ofType: {
+                name: "TypeName",
+                kind: "SCALAR",
+            },
+        };
+        const expected: GqlTypeDef = {
+            name: "TypeName",
+            kind: "SCALAR",
+            isNullable: false,
+            isList: false,
+            isListNullable: true,
+        };
+
+        const typeDef = extractTypeInformation(typeSchema);
+        expect(typeDef).toStrictEqual(expected);
+    });
+
+    it("parses nullable list of nullable scalars", () => {
+        const typeSchema: IntrospectionListTypeRef<IntrospectionScalarType> = {
+            kind: "LIST",
+            ofType: {
+                name: "TypeName",
+                kind: "SCALAR",
+            },
+        };
+        const expected: GqlTypeDef = {
+            name: "TypeName",
+            kind: "SCALAR",
+            isNullable: true,
+            isList: true,
+            isListNullable: true,
+        };
+
+        const typeDef = extractTypeInformation(typeSchema);
+        expect(typeDef).toStrictEqual(expected);
+    });
+
+    it("parses nullable list of non-nullable scalars", () => {
+        const typeSchema: IntrospectionListTypeRef<
+            IntrospectionNonNullTypeRef<IntrospectionScalarType>
+        > = {
+            kind: "LIST",
+            ofType: {
+                kind: "NON_NULL",
+                ofType: {
+                    name: "TypeName",
+                    kind: "SCALAR",
+                },
+            },
+        };
+        const expected: GqlTypeDef = {
+            name: "TypeName",
+            kind: "SCALAR",
+            isNullable: false,
+            isList: true,
+            isListNullable: true,
+        };
+
+        const typeDef = extractTypeInformation(typeSchema);
+        expect(typeDef).toStrictEqual(expected);
+    });
+
+    it("parses non-nullable list of nullable scalars", () => {
+        const typeSchema: IntrospectionNonNullTypeRef<
+            IntrospectionListTypeRef<IntrospectionScalarType>
+        > = {
+            kind: "NON_NULL",
+            ofType: {
+                kind: "LIST",
+                ofType: {
+                    name: "TypeName",
+                    kind: "SCALAR",
+                },
+            },
+        };
+        const expected: GqlTypeDef = {
+            name: "TypeName",
+            kind: "SCALAR",
+            isNullable: true,
+            isList: true,
+            isListNullable: false,
+        };
+
+        const typeDef = extractTypeInformation(typeSchema);
+        expect(typeDef).toStrictEqual(expected);
+    });
 });
